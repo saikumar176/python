faddc696152ebffa046b6a5344dacea27137ac18
Sai Kumar <52398706+saikumar176@users.noreply.github.com>
2019-07-03 09:50:15 +0530

    Update eleven.py

--- eleven.py
+++ eleven.py
@@ -1,6 +1,6 @@
 number = int(input())
-exponent = int(input())
+exp = int(input())
 power = 1
-for i in range(1, exponent + 1):
+for i in range(1, exp + 1):
     power = power * number
 print(power)
